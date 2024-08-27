1b932c1c41f3475a541f17a0007edfde10632982
Daniel Docki <daniel.docki@gmail.com>
2024-08-27 07:53:20 -0300

    Initialize :octocat:

---
+++ lib/phlex/remixicon.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "remixicon/version"
+
+module Phlex
+  module Remixicon
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
